136b792b763f872289d6620aacc4b0c2909e7838
Bernardo_Filho <berna10.fernandes@gmail.com>
2026-04-16 16:23:00 -0300

    feat: add parameter display to main screen body

--- app/src/main/java/br/edu/ifsp/scl/sc3037291/intentscompose/ui/main/MainScreen.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3037291/intentscompose/ui/main/MainScreen.kt
@@ -1,9 +1,13 @@
 package br.edu.ifsp.scl.sc3037291.intentscompose.ui.main
 
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -36,5 +40,18 @@ fun MainScreen(
                 }
             )
         }
-    ) { _ -> }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .padding(24.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text("Parâmetro atual:")
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(parameter, style = MaterialTheme.typography.headlineMedium)
+        }
+    }
 }
