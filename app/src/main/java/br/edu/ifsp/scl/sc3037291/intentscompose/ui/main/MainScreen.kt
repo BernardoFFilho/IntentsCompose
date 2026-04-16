6ae4adedef1d0f843cebf2f6d6354cee123aeb82
Bernardo_Filho <berna10.fernandes@gmail.com>
2026-04-16 16:17:35 -0300

    feat: add main screen scaffold with topbar

--- app/src/main/java/br/edu/ifsp/scl/sc3037291/intentscompose/ui/main/MainScreen.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3037291/intentscompose/ui/main/MainScreen.kt
@@ -1,4 +1,28 @@
 package br.edu.ifsp.scl.sc3037291.intentscompose.ui.main
 
-class MainScreen {
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen(
+    parameter: String,
+    onSetParameter: () -> Unit
+) {
+    var menuExpanded by remember { mutableStateOf(false) }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("IntentsCompose") },
+                actions = {
+                    IconButton(onClick = { menuExpanded = true }) {
+                        Icon(Icons.Default.MoreVert, contentDescription = "Menu")
+                    }
+                }
+            )
+        }
+    ) { _ -> }
 }
